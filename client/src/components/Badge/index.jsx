be8bd4f89640604674ceb8af8786612c8f7309ba
YepileF <yepilef@gmail.com>
2024-06-26 17:53:06 -0500

    feat: badge component

---
+++ client/src/components/Badge/index.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const Badge = ({ children, className = "", ...props }) => {
+  return (
+    <span
+      className={`inline-flex items-center px-2 py-1 border border-gray-400 text-gray-700 rounded-full text-xs font-bold ${className}`}
+      {...props}
+    >
+      {children}
+    </span>
+  );
+};
+
+Badge.propTypes = {
+  children: PropTypes.node.isRequired,
+  className: PropTypes.string,
+};
+
+export default Badge;
