de9e44b26b40bd11f29afe30fd98d6bbf674a392
SantiagoGarces96 <santiagogarcesstory@gmail.com>
2024-06-26 16:10:09 -0500

    Call database getAllRecipes

--- api/src/index.js
+++ api/src/index.js
@@ -1,9 +1,13 @@
+import cors from "cors";
 import express from "express";
 import bodyParser from "body-parser";
 import router from "./routes/index.js";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+app.use(cors({
+  origin: "*",
+}));
 
 // Middleware for parsing application/json
 app.use(bodyParser.json());
--- client/src/layouts/LandingPage/Sections/Hero/index.jsx
+++ client/src/layouts/LandingPage/Sections/Hero/index.jsx
@@ -1,7 +1,43 @@
+import { useEffect, useState } from "react";
 import Card from "../../../../components/Card";
 import Typography from "../../../../components/Typography";
+import { getAllRecipes } from "../../../../api/recepies";
 
 const Hero = () => {
+  const [recipe, setRecipe] = useState(null);
+  useEffect(() => {
+    const fetchRecipe = async () => {
+      try {
+        const recipes = await getAllRecipes();
+        if (recipes && recipes.length > 0) {
+          console.log('First recipe:', recipes[0]); // Verifica la primera receta obtenida
+          setRecipe(recipes[0]); // la primera receta
+        } else {
+          console.log("No recipes found");
+        }
+      } catch (error) {
+        console.error("Error fetching recipe:", error);
+      }
+    };
+
+    fetchRecipe();
+  }, []);
+
+  if (!recipe) {
+    return (
+      <div className="w-full h-[93vh] bg-hero bg-no-repeat bg-cover saturate-[.75]">
+        <div className="w-full p-8">
+          <Typography variant="h2" className="mb-4">
+            Our recommendation
+          </Typography>
+          <Typography variant="body1" className="text-gray-600">
+            Loading...
+          </Typography>
+        </div>
+      </div>
+    );
+  }
+
   return (
     <div className="w-full h-[93vh] bg-hero bg-no-repeat bg-cover saturate-[.75]">
       <div className="w-full p-8">
@@ -9,10 +45,9 @@ const Hero = () => {
           Our recommendation
         </Typography>
         <Card
-          title="Moroccan chickpea and lentil stew"
-          subtitle="Vegan"
-          price="$14.00"
-          imageUrl="https://img.spoonacular.com/recipes/652417-312x231.jpg"
+          title={recipe.title}
+          diets={recipe.diets}
+          imageUrl={recipe.image}
         />
       </div>
     </div>
