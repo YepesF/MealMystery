12e3a21f64e230f9c213a77721c5e9d1f79819df
YepileF <yepilef@gmail.com>
2024-06-26 17:53:25 -0500

    feat: button component

--- client/src/components/Button/index.jsx
+++ client/src/components/Button/index.jsx
@@ -1,13 +1,42 @@
 import PropTypes from "prop-types";
 
-const Button = ({children}) => {
+const Button = ({
+  children,
+  variant = "outlined",
+  size = "medium",
+  className = "",
+  ...props
+}) => {
+  const baseStyle = "border rounded px-4 py-2";
+  const variantStyles = {
+    outlined: "border-gray-400 text-gray-700",
+    primary: "bg-blue-500 text-white",
+    secondary: "bg-gray-500 text-white",
+  };
+  const sizeStyles = {
+    small: "text-sm",
+    medium: "text-base",
+    large: "text-lg",
+  };
+
+  const variantStyle = variantStyles[variant] || variantStyles.outlined;
+  const sizeStyle = sizeStyles[size] || sizeStyles.medium;
+
   return (
-    <div>{children}</div>
+    <button
+      className={`${baseStyle} ${variantStyle} ${sizeStyle} ${className}`}
+      {...props}
+    >
+      {children}
+    </button>
   );
 };
 
 Button.propTypes = {
-    children: PropTypes.node,
+  children: PropTypes.node.isRequired,
+  variant: PropTypes.oneOf(["outlined", "primary", "secondary"]),
+  size: PropTypes.oneOf(["small", "medium", "large"]),
+  className: PropTypes.string,
 };
 
 export default Button;
