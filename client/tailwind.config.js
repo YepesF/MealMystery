5f5e0b7c4e5a1963886089fc9d768d3a44e4efc9
YepileF <yepilef@gmail.com>
2024-07-02 18:37:16 -0500

    feat: color schrema

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -3,6 +3,12 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      colors: {
+        primary: '#F7F6F2',
+        primaryDark: '#2E2E2E',
+        secondary: '#6cb641',
+        accent: '#FF8C00',
+      },
       backgroundImage: {
         hero: "url('https://images.unsplash.com/photo-1533777857889-4be7c70b33f7?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')",
         perks:
