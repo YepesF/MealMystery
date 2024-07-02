4f7866fb03da936f5ad20b81463f332921b02c01
YepileF <yepilef@gmail.com>
2024-07-02 18:51:13 -0500

    feat: badge colors

--- client/src/components/Badge/index.jsx
+++ client/src/components/Badge/index.jsx
@@ -4,7 +4,7 @@ import PropTypes from "prop-types";
 const Badge = ({ children, className = "", ...props }) => {
   return (
     <span
-      className={`inline-flex items-center px-2 py-1 border border-gray-400 text-gray-700 rounded-full text-xs font-bold ${className}`}
+      className={`inline-flex items-center px-2 py-1 border border-gray-400 text-gray-700 rounded-full text-xs font-bold hover:bg-secondary hover:border-transparent hover:text-primary ${className}`}
       {...props}
     >
       {children}
