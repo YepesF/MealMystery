70aff0fc73d53bcf37015c170faddcddcf69df39
YepileF <yepilef@gmail.com>
2024-07-02 19:17:38 -0500

    feat: diets route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,8 @@ import { Routes, Route } from "react-router-dom";
 import LandingPage from "./layouts/LandingPage";
 import RecipePage from "./layouts/RecipePage";
 import RecipesPage from "./layouts/RecipesPage";
+import Diets from "./layouts/Diets";
+import NewRecipe from "./layouts/NewRecipe";
 
 import "./App.css";
 
@@ -15,6 +17,8 @@ function App() {
         <Route path="/" element={<LandingPage />} />
         <Route path="/recipe/:id" element={<RecipePage />} />
         <Route path="/recipes" element={<RecipesPage />} />
+        <Route path="/diets" element={<Diets />} />
+        <Route path="/new" element={<NewRecipe />} />
       </Routes>
     </div>
   );
---
+++ client/src/layouts/Diets/index.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import PropTypes from "prop-types";
+import PageLayout from "../PageLayout";
+
+const Diets = (props) => {
+  return (
+    <PageLayout>
+      <div>Diets</div>
+    </PageLayout>
+  );
+};
+
+Diets.propTypes = {};
+
+export default Diets;
