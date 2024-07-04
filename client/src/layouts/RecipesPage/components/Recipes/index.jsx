17e6f9c4f3f65abc6cf904f14c8bd089ad36f32b
YepileF <yepilef@gmail.com>
2024-07-04 14:59:22 -0500

    fix: height card

--- client/src/layouts/RecipesPage/components/Recipes/index.jsx
+++ client/src/layouts/RecipesPage/components/Recipes/index.jsx
@@ -3,39 +3,38 @@ import { getAllRecipes } from "../../../../api/recepies";
 import Card from "../../../../components/Card";
 
 const RecipesPage = () => {
-    const [recipes, setRecipes] = useState([]);
+  const [recipes, setRecipes] = useState([]);
 
-    useEffect(() => {
-        const fetchRecipes = async () => {
-            try {
-                const fetchedRecipes = await getAllRecipes();
-                setRecipes(fetchedRecipes);
-            } catch (error) {
-                console.error("Error fetching recipes:", error);
-            }
-        };
+  useEffect(() => {
+    const fetchRecipes = async () => {
+      try {
+        const fetchedRecipes = await getAllRecipes();
+        setRecipes(fetchedRecipes);
+      } catch (error) {
+        console.error("Error fetching recipes:", error);
+      }
+    };
 
-        fetchRecipes();
-    }, []);
+    fetchRecipes();
+  }, []);
 
-    return (
-
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 py-8 px-8 w-full">
-            {recipes.map(({id, title, diets, image}, index) => (
-                <Card
-                    key={id}
-                    id={id}
-                    title={title}
-                    diets={diets}
-                    imageUrl={image}
-                    onClick={() => handleCardClick(id)}
-                    index={index}
-                />
-            ))}
-        </div>
-        
-        
-    );
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 py-8 px-8 w-full">
+      {recipes.map(({ id, title, diets, image, ready_in_minutes }, index) => (
+        <Card
+          key={id}
+          id={id}
+          title={title}
+          diets={diets}
+          imageUrl={image}
+          readyIn={ready_in_minutes}
+          onClick={() => handleCardClick(id)}
+          index={index}
+          imageHeight={60}
+        />
+      ))}
+    </div>
+  );
 };
 
 export default RecipesPage;
