8015a0772e6ad0dd3cfadf11e1fe18e897fa76a0
YepileF <yepilef@gmail.com>
2024-07-13 11:18:08 -0500

    feat: diets from landing page

--- client/src/api/recepies.js
+++ client/src/api/recepies.js
@@ -3,9 +3,8 @@ import axios from "axios";
 import { ROUTES, URLS } from "../constants/index";
 export const getAllRecipes = async (page, column, sortType) => {
   try {
-    const response = await axios.get(
-      `${URLS.API}?page=${page}&column=${column}&sortType=${sortType}`
-    );
+    const url = `${URLS.API}?page=${page}&column=${column}&sortType=${sortType}`;
+    const response = await axios.get(url);
 
     if (response.status !== 200) {
       throw new Error("Network response was not ok");
@@ -33,7 +32,9 @@ export const getRecipeById = async (id) => {
 
 export const searchRecipe = async (title, page, diet = "") => {
   try {
-    const response = await axios.get(`${URLS.API}/search?page=${page}&diet=${diet}&title=${title}&limit=12`);
+    const response = await axios.get(
+      `${URLS.API}/search?page=${page}&diet=${diet}&title=${title}&limit=12`
+    );
 
     if (response.status !== 200) {
       throw new Error("Network response was not ok");
@@ -47,7 +48,7 @@ export const searchRecipe = async (title, page, diet = "") => {
 
 export const getDiets = async () => {
   try {
-    const response = await axios.get(`${URLS.API}/${ROUTES.DIETS}`);
+    const response = await axios.get(`${URLS.API}${ROUTES.DIETS}`);
     return response.data;
   } catch (error) {
     console.error("Error fetching diets:", error);
@@ -55,9 +56,17 @@ export const getDiets = async () => {
   }
 };
 
-export const recipesByDiet = async (diet, page = 1, limit = 10, column, sortType) => {
+export const recipesByDiet = async (
+  diet,
+  page = 1,
+  limit = 12,
+  column,
+  sortType
+) => {
   try {
-    const response = await axios.get(`${URLS.API}${ROUTES.DIET}?diet=${diet}&page=${page}&limit=${limit}&column=${column}&sortType=${sortType}`);
+    const response = await axios.get(
+      `${URLS.API}${ROUTES.DIET}?diet=${diet}&page=${page}&limit=${limit}&column=${column}&sortType=${sortType}`
+    );
     return response.data;
   } catch (error) {
     console.error("Error fetching recipes by diet:", error);
