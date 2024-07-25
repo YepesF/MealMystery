c9be9362bf4e967b48f165a6cd8e4b9ad34493e2
YepileF <yepilef@gmail.com>
2024-07-25 18:35:57 -0500

    feat: chips by filters

--- client/src/components/Filters/index.jsx
+++ client/src/components/Filters/index.jsx
@@ -5,12 +5,14 @@ import ReadyInMinutesFilters from "./components/ReadyInMinutesFilters";
 import HealthScoreFilters from "./components/HealthScoreFilters";
 import SpoonacularScoreFilters from "./components/SpoonacularScoreFilters";
 import { motion } from "framer-motion";
+import FilterChip from "../FilterChip";
 
 const Filters = ({
   filterCount,
   clearFilters,
   selectedDiets,
   healthScore,
+  readyInMinutes,
   handleSelectedDiets,
   handleRangeChange,
   setReadyInMinutes,
@@ -18,17 +20,13 @@ const Filters = ({
   spoonacularScore,
   setSpoonacularScore,
 }) => {
-  const variants = {
-    visible: { opacity: 0 },
-    hidden: { opacity: 1 },
-  };
   return (
     <motion.div
       initial={{ opacity: 0 }}
       animate={{ opacity: 1, x: [-500, 0] }}
       exit={{ opacity: 0, x: [0, -500] }}
       transition={{ ease: "easeOut", duration: 0.4 }}
-      className="bg-primary h-full border-r border-t  border-black overflow-hidden pr-2"
+      className="bg-primary h-[80vh] pr-2 overflow-auto overflow-x-hidden"
     >
       <div className="py-4 pr-4 flex justify-between items-center min-h-[6vh]">
         <Typography variant="h1" className="text-2xl">
@@ -45,12 +43,43 @@ const Filters = ({
           </Button>
         )}
       </div>
+      {filterCount > 0 && (
+        <div className="w-full flex flex-wrap gap-1">
+          {selectedDiets.length > 0 &&
+            selectedDiets.map((diet, index) => (
+              <FilterChip
+                key={index}
+                value={diet}
+                handle={() => handleSelectedDiets(diet)}
+              />
+            ))}
+          {(readyInMinutes.from > 0 || readyInMinutes.to > 0) && (
+            <FilterChip
+              value={`Ready in: ${readyInMinutes.from} - ${readyInMinutes.to} min.`}
+              handle={() => setReadyInMinutes({ from: 0, to: 0 })}
+            />
+          )}
+          {healthScore.to > 0 && (
+            <FilterChip
+              value={`Health score: ${healthScore.from} - ${healthScore.to}`}
+              handle={() => setHealthScore({ from: 0, to: 0 })}
+            />
+          )}
+          {spoonacularScore.to > 0 && (
+            <FilterChip
+              value={`Spoonacular score: ${spoonacularScore.from} - ${spoonacularScore.to}`}
+              handle={() => setSpoonacularScore({ from: 0, to: 0 })}
+            />
+          )}
+        </div>
+      )}
       <DietsFilters
         handleSelectedDiets={handleSelectedDiets}
         selectedDiets={selectedDiets}
       />
       <ReadyInMinutesFilters
         handleRangeChange={handleRangeChange}
+        readyInMinutes={readyInMinutes}
         setReadyInMinutes={setReadyInMinutes}
       />
       <HealthScoreFilters
