9e5dc6af65f06922c26b5c2a599f49d2870a31ff
YepileF <yepilef@gmail.com>
2024-08-08 16:35:37 -0500

    responsive: mobile footer

--- client/src/components/Footer/index.jsx
+++ client/src/components/Footer/index.jsx
@@ -5,18 +5,24 @@ import { FaGithub } from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <section className="flex h-[60vh] w-full flex-col gap-8 bg-black p-4">
-      <div className="flex h-[30vh] w-full flex-col justify-between rounded-md bg-secondary p-4">
+    <section className="flex w-full flex-col items-center justify-center px-2 pt-20 md:px-4 md:pt-32 2k:px-8 2k:pt-48">
+      <div className="flex h-[20vh] w-full flex-col justify-between rounded-md bg-secondary p-4 md:h-[30vh]">
         <div className="flex w-full items-start justify-between">
           <Typography className="text-xs text-primary" variant="body2">
             ↓
           </Typography>
-          <Typography className="text-xs text-primary" variant="body2">
+          <Typography
+            className="text-[8px] text-primary md:text-xs"
+            variant="body2"
+          >
             DISCOVER THE <br />
             JOY OF COOKING <br />
             WITH US.
           </Typography>
-          <Typography className="text-xs text-primary" variant="body2">
+          <Typography
+            className="text-[8px] text-primary md:text-xs"
+            variant="body2"
+          >
             YOUR TRUSTED <br />
             PARTNER IN THE <br />
             KITCHEN.
@@ -26,7 +32,10 @@ const Footer = () => {
           </Typography>
         </div>
         <div className="flex w-full flex-col justify-center gap-4">
-          <Typography className="text-6xl text-primary" variant="h1">
+          <Typography
+            className="text-4xl text-primary md:text-6xl"
+            variant="h1"
+          >
             Meal Mystery
           </Typography>
           <div className="h-[1vh] w-full rounded-sm bg-primary"></div>
@@ -36,7 +45,7 @@ const Footer = () => {
         <div className="flex w-full flex-col items-start justify-start border-b border-r border-primary p-4">
           <a href={ROUTES.RECIPES}>
             <Typography
-              className="text-2xl font-extrabold text-primary hover:text-accent"
+              className="font-extrabold text-primary hover:text-accent md:text-2xl"
               variant="body2"
             >
               Recepies
@@ -44,7 +53,7 @@ const Footer = () => {
           </a>
           <a href={ROUTES.NEW}>
             <Typography
-              className="text-2xl font-extrabold text-primary hover:text-accent"
+              className="font-extrabold text-primary hover:text-accent md:text-2xl"
               variant="body2"
             >
               New Recipe
@@ -53,7 +62,7 @@ const Footer = () => {
         </div>
         <div className="w-full border-b border-primary p-4">
           <Typography
-            className="text-sm text-primary xl:w-full"
+            className="text-[8px] text-primary md:text-sm xl:w-full"
             variant="body2"
           >
             Meal Mystery is a comprehensive platform designed for culinary
@@ -64,13 +73,13 @@ const Footer = () => {
           </Typography>
         </div>
       </div>
-      <div className="flex justify-center">
+      <div className="my-3 flex justify-center">
         <a
           href="https://github.com/YepesF/MealMystery"
           target="_blank"
           rel="noopener noreferrer"
         >
-          <FaGithub className="h-8 w-8 text-primary" />
+          <FaGithub className="h-8 w-8 text-primary hover:fill-accent" />
         </a>
       </div>
     </section>
