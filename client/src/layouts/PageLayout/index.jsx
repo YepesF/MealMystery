6d272feb2f5967790e6d3166234293f067b46ac4
YepileF <yepilef@gmail.com>
2024-08-08 16:24:56 -0500

    fix: footer

--- client/src/layouts/PageLayout/index.jsx
+++ client/src/layouts/PageLayout/index.jsx
@@ -24,7 +24,7 @@ const PageLayout = ({ children, className }) => {
       <NavBar isOpen={isOpen} handleOpenMenu={handleOpenMenu} />
       <DrawerMenu isMenuOpen={isMenuOpen} handleCloseMenu={handleCloseMenu} />
       <div className={`${baseStyle} ${className}`}>{children}</div>
-      {/* <Footer /> */}
+      <Footer />
     </div>
   );
 };
