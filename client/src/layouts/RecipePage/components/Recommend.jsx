388c0a5caadde768393704a96c4404b20062d687
YepileF <yepilef@gmail.com>
2024-08-09 02:20:43 -0500

    responsive: mobile and tablet recipe

--- client/src/layouts/RecipePage/components/Recommend.jsx
+++ client/src/layouts/RecipePage/components/Recommend.jsx
@@ -4,49 +4,57 @@ import { Link } from "react-router-dom";
 import { ROUTES } from "../../../constants";
 import Typography from "../../../components/Typography";
 import Badge from "../../../components/Badge";
+import useScreenSize from "../../../hooks/useScreenSize";
 
 const Recommend = ({ loading, recipes }) => {
+  const screenSize = useScreenSize();
   return (
-    <div id="recommend" className="my-[6vh] min-h-[50rem] w-full">
+    <div
+      id="recommend"
+      className="mt-[6vh] w-full p-2 dark:text-primary hd:min-h-[50rem]"
+    >
       {loading && (
         <div className="flex h-[40rem] items-center justify-center">
           <Spinner color="red" className="h-16 w-16 text-accent" />
         </div>
       )}
-      <div className="h-full w-full">
+      <div className="flex h-full w-full flex-col items-center justify-center hd:items-start">
         <Typography
-          className="text-ellipsis whitespace-nowrap p-4 text-6xl font-extrabold capitalize"
+          className="text-ellipsis whitespace-nowrap p-4 text-3xl font-extrabold capitalize hd:text-6xl"
           variant="h2"
         >
           We recommend for you
         </Typography>
-        <div className="flex h-full items-center justify-start">
+        <div className="flex h-full flex-col items-center justify-start hd:flex-row">
           {!loading &&
             recipes.map(
               ({ id, image, title, ready_in_minutes, diets }, index) => (
                 <Link
                   key={index}
                   to={`${ROUTES.RECIPE}/${id}`}
-                  className="w-1/3 flex-shrink-0 p-0"
+                  className="flex-shrink-0 p-0 md:w-full hd:w-1/3"
                 >
                   <article
-                    className={`h-full border-t p-4 ${index < recipes.length - 1 ? "border-r" : ""} flex flex-col items-start justify-start gap-6 border-gray-400 bg-primary px-4`}
+                    className={`h-full border-t p-4 ${screenSize > 1280 && index < recipes.length - 1 ? "border-r" : ""} flex flex-col items-start justify-start gap-6 border-gray-400 bg-primary dark:bg-primaryDark dark:text-primary hd:px-4`}
                   >
                     <div className="w-full">
                       <Typography
                         variant="body1"
-                        className="overflow-hidden text-ellipsis whitespace-nowrap text-xl font-extrabold"
+                        className="text-ellipsis whitespace-nowrap font-extrabold md:text-xl hd:text-xl"
                       >
                         {title}
                       </Typography>
-                      <Typography variant="body1" className="capitalize">
+                      <Typography
+                        variant="body1"
+                        className="text-xs capitalize md:text-sm 2k:text-xl"
+                      >
                         Ready in minutes:{" "}
                         <strong className="ml-2 text-accent">
                           {ready_in_minutes}
                         </strong>
                       </Typography>
                     </div>
-                    <div className="h-[40rem] w-full flex-shrink-0">
+                    <div className="w-full flex-shrink-0 hd:h-[40rem]">
                       <img
                         className="h-full w-full object-cover"
                         src={image}
